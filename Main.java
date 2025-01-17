ec714991e30a1fc5bb3718f86ff815ea9941c548
tramngyn <bvnguyen115@student.hbuhsd.edu>
2025-01-17 03:44:58 +0000

    finished problem 1

--- Main.java
+++ Main.java
@@ -4,6 +4,17 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter any numbers. (Enter -1 to stop)");
+    int num = 0;
+    int total = 0;
+    
+    while (num != -1))
+    {
+      num = sc.nextInt();
+      sum += num;
+    }
+
+    System.out.println("The sum is " + sum ".");
   }
 }
